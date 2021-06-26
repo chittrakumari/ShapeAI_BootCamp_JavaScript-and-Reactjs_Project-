c98ed0dfdfce4fc49c264cedc9d49fdea87f391a
Chittra Kumari <chittra.kumari_cs.ccv19@gla.ac.in>
2021-06-26 17:25:29 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1>Shape AI Bootcamps</h1>
+      </header>
+    </div>
+  );
+}
+
+module.exports = Header;
